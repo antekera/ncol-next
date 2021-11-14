e3b424c5809b3eaed8a45c14fed6fec6714ca794
Miguel Antequera <miguel.antequera@mercadolibre.cl>
2021-11-13 21:54:13 -0300

    Add nextjs and config files

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      gray: colors.coolGray,
+      blue: colors.sky,
+      orange: colors.orange,
+    },
+    fontFamily: {
+      sans: ['Lato', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        128: '32rem',
+        144: '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      },
+    },
+  },
+}
