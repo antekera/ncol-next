57d886ae4a8584754a5f36433f5f85b1e1148804
Miguel Antequera <miguel.antequera@mercadolibre.cl>
2021-11-13 23:46:35 -0300

    Add document page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,6 @@ const Home: NextPage = () => {
     <div className='flex flex-col items-center justify-center min-h-screen py-2'>
       <Head>
         <title>Create Next App</title>
-        <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <main className='flex flex-col items-center justify-center w-full flex-1 px-20 text-center'>
