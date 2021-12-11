fd4f299b3e0832c16f9c88c5f44d423179ca3480
Miguel Antequera <miguel.antequera@mercadolibre.cl>
2021-12-11 13:15:52 -0300

    update next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: ['i0.wp.com', 'i1.wp.com', 'i2.wp.com' ,'secure.gravatar.com'],
+    formats: ['image/avif', 'image/webp'],
+  },
 }
