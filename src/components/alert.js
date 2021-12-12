5da0a829eb24d3b7dcf81f98efb2613c5e748f8c
Miguel Antequera <miguel.antequera@mercadolibre.cl>
2021-12-12 18:38:28 -0300

    fix import

--- src/components/alert.js
+++ src/components/alert.js
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import Container from './container'
 import cn from 'classnames'
-import { EXAMPLE_PATH } from '../lib/constants'
+import { CMS_URL } from '../lib/constants'
 
 export default function Alert({ preview }) {
   return (
@@ -28,7 +28,7 @@ export default function Alert({ preview }) {
             <>
               The source code for this blog is{' '}
               <a
-                href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
+                href={`https://github.com/vercel/next.js/tree/canary/examples/${CMS_URL}`}
                 className='underline hover:text-success duration-200 transition-colors'
               >
                 available on GitHub
