fb5440c0410f8fe8d025046ec69298d21c62b675
Miguel Antequera <miguel.antequera@mercadolibre.cl>
2021-12-14 23:17:45 -0300

    modify grid

--- src/components/more-stories.js
+++ src/components/more-stories.js
@@ -6,7 +6,7 @@ export default function MoreStories({ posts }) {
       <h2 className="mb-8 text-6xl font-bold leading-tight tracking-tighter md:text-7xl">
         More Stories
       </h2>
-      <div className="mb-32 grid grid-cols-1 md:grid-cols-2 md:col-gap-16 lg:col-gap-32 row-gap-20 md:row-gap-32">
+      <div className="mb-32 grid grid-cols-2 md:grid-cols-3 md:col-gap-16 lg:col-gap-32 row-gap-20 md:row-gap-32">
         {posts.map(({ node }) => (
           <PostPreview
             key={node.slug}
