7441d0e558c521dae4631c5aca1f9e74d39068d8
Miguel Antequera <miguel.antequera@mercadolibre.cl>
2022-02-13 11:46:08 -0300

    add post header cat and summary

--- src/components/PostHeader/index.tsx
+++ src/components/PostHeader/index.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 import { Date, Container, PostTitle } from 'components/'
 import Categories from 'components/categories'
 import { PostHeader as PostHeaderProps } from 'lib/types'
@@ -7,7 +9,20 @@ const defaultProps = {}
 const PostHeader = ({ title, date, categories }: PostHeaderProps) => {
   return (
     <Container>
+      <Link href={'@!'}>
+        <a
+          className='relative inline-block px-1 pt-1 text-xs leading-none text-white uppercase rounded top-4 bg-primary hover:bg-secondary ease-in-out duration-200 border-primary hover:border-secondary sm:hidden pb-[3px]'
+          aria-label={'Costa Oriental'}
+        >
+          Costa Oriental
+        </a>
+      </Link>
       <PostTitle title={title} />
+      <p className='mb-6 -mt-2 md:text-xl md:-mt-7 text-slate-500 md:mb-12 leading-5 md:leading-6 sm:w-11/12'>
+        “It’s definitely a more complicated Olympics,” said American snowboarder
+        Jamie Anderson.
+      </p>
+
       <div className='max-w-2xl'>
         <div className='mb-6 text-sm md:text-lg'>
           Posted <Date dateString={date} />
