4eb8adc927c04b43b381db38567048d5a6c37d32
Miguel Antequera <miguel.antequera@mercadolibre.cl>
2022-06-26 22:08:37 -0400

    static pages

---
+++ src/components/LegalPage/index.tsx
@@ -0,0 +1,35 @@
+import { NextPage } from 'next'
+import Head from 'next/head'
+
+import { Container, Layout } from 'components'
+
+import styles from './style.module.css'
+
+type LegalPageProps = {
+  children: React.ReactNode
+  title: string
+}
+
+const LegalPage: NextPage<LegalPageProps> = ({ title, children }) => {
+  return (
+    <>
+      <Layout>
+        <Head>
+          <title>{title}</title>
+        </Head>
+        <Container className='py-12'>
+          <div className='xl:w-5/6'>
+            <h1 className='mb-4 text-3xl md:text-4xl'>{title}</h1>
+            <section
+              className={`text-slate-800 font-sans_light ${styles.content}`}
+            >
+              {children}
+            </section>
+          </div>
+        </Container>
+      </Layout>
+    </>
+  )
+}
+
+export { LegalPage }
