0c73f7c7b02dca798a839691212c26e76b0bfea1
Miguel Antequera <miguel.antequera@mercadolibre.cl>
2022-08-13 16:09:58 -0400

    report web vitals to analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -38,11 +38,18 @@ const App: React.FC<AppProps> = ({ Component, pageProps, err }) => {
 export function reportWebVitals({
   id,
   name,
-  value,
-  startTime
+  label,
+  value
 }: NextWebVitalsMetric) {
-  // eslint-disable-next-line no-console
-  console.log(`${name}: `, id, value, startTime) // The metric object ({ id, name, startTime, value, label }) is logged to the console
+  if (window && window.gtag) {
+    window.gtag('event', name, {
+      event_category:
+        label === 'web-vital' ? 'Web Vitals' : 'Next.js custom metric',
+      value: Math.round(name === 'CLS' ? value * 1000 : value),
+      event_label: id,
+      non_interaction: true
+    })
+  }
 }
 
 export default App
