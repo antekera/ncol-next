b558af26801708167c131eda00f8d9929034fae0
Miguel Antequera <miguel.antequera@mercadolibre.cl>
2022-08-21 18:31:02 -0400

    slot height

--- src/components/AdSlot/index.tsx
+++ src/components/AdSlot/index.tsx
@@ -8,13 +8,7 @@ const AdSlot = ({ id }: GAdsScriptProps) => {
   const tag = `div-gpt-ad-${id}`
   useDfpSlots(tag)
 
-  return (
-    <div
-      id={tag}
-      className='mb-4 bloque-adv'
-      style={{ width: '300px', height: '250px' }}
-    ></div>
-  )
+  return <div id={tag} className='mb-4 bloque-adv' />
 }
 
 export { AdSlot }
