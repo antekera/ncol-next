60d2d39d9d29cffbe48e264c40a0dff06109acfc
Miguel Antequera <miguel.antequera@mercadolibre.cl>
2022-08-24 09:29:27 -0400

    remove txt

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -23,24 +23,18 @@ const App: React.FC<AppProps> = ({ Component, pageProps, err }) => {
       TagManager.initialize(tagManagerArgs)
 
       // @ts-ignore
-      if (!window.adsbygoogle) {
-        // @ts-ignore
-        ;(window.adsbygoogle = window.adsbygoogle || []).push({})
-      }
+      ;(window.adsbygoogle = window.adsbygoogle || []).push({})
 
       // @ts-ignore
-      if (!window._taboola) {
-        // @ts-ignore
-        window._taboola = window._taboola || []
+      window._taboola = window._taboola || []
 
-        // @ts-ignore
-        window._taboola.push({
-          mode: 'thumbnails-a',
-          container: 'taboola-below-article-thumbnails',
-          placement: 'Below Article Thumbnails',
-          target_type: 'mix'
-        })
-      }
+      // @ts-ignore
+      window._taboola.push({
+        mode: 'thumbnails-a',
+        container: 'taboola-below-article-thumbnails',
+        placement: 'Below Article Thumbnails',
+        target_type: 'mix'
+      })
     }
   }, [])
 
