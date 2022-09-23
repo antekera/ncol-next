2406db4d5143d6d8746f80f4fc952a74de778394
Miguel Antequera <miguel.antequera@mercadolibre.cl>
2022-09-22 21:04:53 -0300

    fix icon size

--- src/components/ButtonGoTop/index.tsx
+++ src/components/ButtonGoTop/index.tsx
@@ -7,8 +7,10 @@ const ButtonGoTop = () => {
       onClick={goToTop}
       className='absolute text-white border-solid border-none rounded cursor-pointer h-9 w-9 link-go-top hover:-top-4 -top-3 right-6 bg-primary ease-in duration-150'
     >
-      <span className='inline-block mt-3 text-3xl material-symbols-rounded leading-4'>
-        keyboard_control_key
+      <span className='text-3xl'>
+        <span className='inline-block mt-3 text-3xl material-symbols-rounded leading-4'>
+          keyboard_control_key
+        </span>
       </span>
     </button>
   )
