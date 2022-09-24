d27ad2009adf158e9ae8b45d60b50a5df54fd491
Miguel Antequera <miguel.antequera@mercadolibre.cl>
2022-09-24 15:40:14 -0300

    add taboola

--- src/pages/[posts]/[month]/[day]/[slug].tsx
+++ src/pages/[posts]/[month]/[day]/[slug].tsx
@@ -40,6 +40,44 @@ const Post: NextPage<PostPage> = ({ post, ads }) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isLoading])
 
+  useEffect(() => {
+    if (!window) {
+      // @ts-ignore
+      window._taboola = window._taboola || []
+      // @ts-ignore
+      _taboola.push({ article: 'auto' })
+
+      // @ts-ignore
+      window._taboola.push({
+        mode: 'thumbnails-a',
+        container: 'taboola-below-article-thumbnails',
+        placement: 'Below Article Thumbnails',
+        target_type: 'mix'
+      })
+    }
+
+    // @ts-ignore
+    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+    !(function (e, f, u, i) {
+      if (!document.getElementById(i)) {
+        // @ts-ignore
+        e.async = 1
+        e.src = u
+        e.id = i
+        // @ts-ignore
+        f.parentNode.insertBefore(e, f)
+      }
+    })(
+      document.createElement('script'),
+      document.getElementsByTagName('script')[0],
+      '//cdn.taboola.com/libtrc/noticiascol-noticiascol/loader.js',
+      'tb_loader_script'
+    )
+    if (window.performance && typeof window.performance.mark == 'function') {
+      window.performance.mark('tbl_ic')
+    }
+  }, [])
+
   if (isLoading) {
     return <LoadingPage />
   }
