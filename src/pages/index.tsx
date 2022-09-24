2510417b43b52011fa766df94b26fb25b8a9f8c5
Miguel Antequera <miguel.antequera@mercadolibre.cl>
2022-09-24 12:27:14 -0300

    add sitemap

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -97,7 +97,9 @@ const Index: NextPage<HomePage> = ({
               />
             </div>
           </div>
-          <div className='p-2 mb-10 md:flex md:ml-0 bg-slate-100'></div>
+          <div className='p-2 mb-10 md:flex md:ml-0 bg-slate-100'>
+            <AdDfpSlot id={ads.sidebar.id} className='mb-4 show-mobile' />
+          </div>
           <div className='mb-10 -ml-1 md:flex md:mt-4 md:ml-0'>
             <div className='flex-none md:w-3/5 md:pl-5 md:pr-3'>
               <LeftPosts posts={leftPosts_4} />
