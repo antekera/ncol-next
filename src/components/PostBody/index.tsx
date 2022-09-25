428866daf677a0998fc0daddc42425ce0b6b10de
Miguel Antequera <miguel.antequera@mercadolibre.cl>
2022-09-24 21:22:32 -0300

    add taboola script (#34)

--- src/components/PostBody/index.tsx
+++ src/components/PostBody/index.tsx
@@ -5,23 +5,17 @@ import { PostBodyProps } from 'lib/types'
 
 import styles from './style.module.css'
 
-const PostBody = ({ content, ads }: PostBodyProps) => {
-  const regex = /<*>(.*?)<\/p>/
-  const p = '</p>'
-  const [first] = content.split(p)
-  const firstParagraphContent = `${first}${p}`
-  const restParagraphContent = content.replace(regex, '')
-
+const PostBody = ({ firstParagraph, secondParagraph, ads }: PostBodyProps) => {
   return (
     <>
       <div
         className={`${styles.capital} ${styles.content} max-w-2xl mx-auto capital-letter`}
-        dangerouslySetInnerHTML={{ __html: firstParagraphContent }}
+        dangerouslySetInnerHTML={{ __html: firstParagraph }}
       />
       <AdDfpSlot id={ads.squareC1.id} className='pt-2 mb-2' />
       <div
         className={`${styles.content} max-w-2xl pb-6 mx-auto`}
-        dangerouslySetInnerHTML={{ __html: restParagraphContent }}
+        dangerouslySetInnerHTML={{ __html: secondParagraph }}
       />
     </>
   )
