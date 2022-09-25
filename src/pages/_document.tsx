758c39a96e88c48750adae8db880bca198c0b86d
Miguel Antequera <miguel.antequera@mercadolibre.cl>
2022-09-25 12:12:02 -0300

    block font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,7 +10,7 @@ class AppDocument extends Document {
           {/* Icons */}
           <link
             rel='stylesheet'
-            href='https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=optional'
+            href='https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=block'
           />
           {/* Favicon */}
           <link
