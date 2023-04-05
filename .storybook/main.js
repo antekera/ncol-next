8b14fb9262847d4eaac857415f85be654f74ec24
Miguel Antequera <miguel.antequera@maritz.com>
2023-04-05 09:45:49 -0400

    chore: update storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,25 +1,20 @@
 module.exports = {
-  stories: [
-    '../src/**/*.stories.mdx',
-    '../src/components/**/*.stories.@(js|jsx|ts|tsx)',
-  ],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-a11y',
-    '@storybook/addon-outline',
-    '@storybook/addon-measure',
-    '@storybook/addon-postcss',
-    'storybook-tailwind-dark-mode',
-  ],
+  stories: ['../src/**/*.stories.mdx', '../src/components/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-a11y', '@storybook/addon-outline', '@storybook/addon-measure', '@storybook/addon-postcss', 'storybook-tailwind-dark-mode', '@storybook/addon-mdx-gfm'],
   typescript: {
     check: false,
     checkOptions: {},
     reactDocgen: 'react-docgen-typescript',
     reactDocgenTypescriptOptions: {
       shouldExtractLiteralValuesFromEnum: true,
-      propFilter: prop =>
-        prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
-    },
+      propFilter: prop => prop.parent ? !/node_modules/.test(prop.parent.fileName) : true
+    }
   },
-}
+  framework: {
+    name: '@storybook/nextjs',
+    options: {}
+  },
+  docs: {
+    autodocs: true
+  }
+};
