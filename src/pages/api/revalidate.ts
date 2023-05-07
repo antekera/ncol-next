fbf9a9e4f9df6de1d0695080e92b805b9856a8c3
Miguel Antequera <miguel.antequera@maritz.com>
2023-05-07 19:06:57 -0400

    check string path

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -7,8 +7,12 @@ export default async function handler(
   if (req.query.secret !== process.env.REVALIDATE_KEY) {
     return res.status(401).json({ message: 'Invalid token ' })
   }
-
   const path = req.query.path as string
+
+  if (typeof path === 'string') {
+    return res.status(401).json({ message: 'Invalid path ' })
+  }
+
   await res.revalidate(path)
   return res.json({ revalidated: true })
 }
