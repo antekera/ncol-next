bdc79415b9c102b15cca2ac0ae65049b8f62b231
Miguel Antequera <miguel.antequera@maritz.com>
2023-11-05 19:31:45 -0300

    Add authorNotIn filter to post queries

--- src/lib/api/getPostsPerCategory/query.ts
+++ src/lib/api/getPostsPerCategory/query.ts
@@ -2,7 +2,7 @@ export const query = `
 query PostsPerCategory($slug: String!, $qty: Int!) {
   posts(
     first: $qty, after: null, last: null, before: null
-    where: {dateQuery: {after: {month: 1}} ,orderby: {field: DATE, order: DESC}, categoryName: $slug, status: PUBLISH}
+    where: {dateQuery: {after: {month: 1}} ,orderby: {field: DATE, order: DESC}, categoryName: $slug, status: PUBLISH, authorNotIn: "1"}
   ) {
     edges {
     cursor
