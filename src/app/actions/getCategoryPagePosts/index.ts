a4d32e8eda28ea9bc3e738b9f2e02a89677e10e6
Miguel Antequera <miguel.antekera@gmail.com>
2024-09-02 20:28:51 -0400

    fix: remove babel (#87)
    * fix: remove babel
    
    * fix: test logs DELETE

--- src/app/actions/getCategoryPagePosts/index.ts
+++ src/app/actions/getCategoryPagePosts/index.ts
@@ -1,5 +1,6 @@
 'use server'
 
+import { log } from '@logtail/next'
 import { unstable_cache as cache } from 'next/cache'
 
 import { fetchAPI } from '@app/actions/fetchAPI'
@@ -14,6 +15,17 @@ export const getCategoryPagePosts = cache(
     qty: number,
     endCursor: string
   ): Promise<PostsCategoryQueried> => {
+    // eslint-disable-next-line no-console
+    console.log(`GETCATEGORYPAGEPOSTS`, {
+      slug,
+      qty,
+      endCursor
+    })
+    log.info(`GETCATEGORYPAGEPOSTS`, {
+      slug,
+      qty,
+      endCursor
+    })
     const { posts } =
       (await fetchAPI({
         revalidate: TIME_REVALIDATE.DAY,
@@ -24,6 +36,13 @@ export const getCategoryPagePosts = cache(
           endCursor
         }
       })) ?? {}
+    log.info(`POSTS`, {
+      posts
+    })
+    // eslint-disable-next-line no-console
+    console.log(`POSTS`, {
+      posts
+    })
     return posts
   },
   ['data-category'],
