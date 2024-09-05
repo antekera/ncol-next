b1ad94fdc3e81572b4da07c066ad278b08788d8f
antekera <miguel.antekera@gmail.com>
2024-09-05 10:41:20 -0400

    fix: revalidate path

--- src/app/actions/revalidate.ts
+++ src/app/actions/revalidate.ts
@@ -4,6 +4,16 @@ import { log } from '@logtail/next'
 import { revalidatePath } from 'next/cache'
 
 export const revalidateDataPath = async (path: string) => {
-  log.info(`Revalidating path: ${path}`)
-  revalidatePath(path, 'page')
+  try {
+    await revalidatePath(path, 'page')
+    log.info(`REVALIDATION_SUCCESSFUL: ${path}`)
+    return { success: true, message: `Successfully revalidated path: ${path}` }
+  } catch (error) {
+    log.error(`REVALIDATION_FAILED: ${path}`, { error })
+    return {
+      success: false,
+      message: `Failed to revalidate path: ${path}`,
+      error
+    }
+  }
 }
