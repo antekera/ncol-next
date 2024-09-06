153573c117fc29d565ccbb6ce9d26d1cede65d6c
antekera <miguel.antekera@gmail.com>
2024-09-06 10:42:50 -0400

    fix: remove unstable cache

--- src/app/actions/getAllCategoriesWithSlug/index.ts
+++ src/app/actions/getAllCategoriesWithSlug/index.ts
@@ -1,15 +1,12 @@
 'use server'
 
-import { unstable_cache as cache } from 'next/cache'
-
 import { fetchAPI } from '@app/actions/fetchAPI'
 import { TIME_REVALIDATE } from '@lib/constants'
 import { CategoriesPath } from '@lib/types'
 
 import { query } from './query'
 
-export const getAllCategoriesWithSlug =
-  cache(async (): Promise<CategoriesPath> => {
-    const data = await fetchAPI({ query, revalidate: TIME_REVALIDATE.DAY })
-    return data?.categories
-  }, ['data-category'])
+export const getAllCategoriesWithSlug = async (): Promise<CategoriesPath> => {
+  const data = await fetchAPI({ query, revalidate: TIME_REVALIDATE.DAY })
+  return data?.categories
+}
