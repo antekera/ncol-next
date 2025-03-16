fb7e97a8c3ce989a95afb546f9e752f7c5be685e
antekera <miguel.antekera@gmail.com>
2025-03-16 16:15:22 -0300

    fix: update

--- sst.config.ts
+++ sst.config.ts
@@ -1,6 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference path="./.sst/platform/config.d.ts" />
-
 export default $config({
   app(input) {
     return {
@@ -12,13 +11,6 @@ export default $config({
   },
   async run() {
     new sst.aws.Nextjs(`${process.env.APP_NAME}`, {
-      domain: {
-        name: `${process.env.DOMAIN_NAME}`,
-        redirects: [` www.${process.env.DOMAIN_NAME}/*`],
-        // dns: sst.aws.dns(),
-        cert: `${process.env.CERT_ARN}`,
-        aliases: [`cdn.${process.env.DOMAIN_NAME}`]
-      },
       invalidation: {
         paths: ['/2025/*', '/2026/*'],
         wait: false
