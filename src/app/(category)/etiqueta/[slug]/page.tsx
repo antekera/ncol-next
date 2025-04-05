a1be01c2945e776da3ad252d076b0bc3275b8f34
antekera <miguel.antekera@gmail.com>
2025-04-04 23:26:58 -0300

    feat: add useTagPosts hook and TagPosts component
    Introduce a new custom hook  to fetch tag-specific posts and a corresponding  component to display them. This refactoring improves code modularity and reusability by separating data fetching logic from the UI rendering. The  option is also added to the page to force static rendering for better performance.

--- src/app/(category)/etiqueta/[slug]/page.tsx
+++ src/app/(category)/etiqueta/[slug]/page.tsx
@@ -1,23 +1,17 @@
-export const revalidate = 10800 // 3 horas
+export const dynamic = 'force-static'
 
-import { Fragment, Suspense } from 'react'
+import { Suspense } from 'react'
 import { getAllTagsWithSlug } from '@app/actions/getAllTagsWithSlug'
-import { getTagPagePosts } from '@app/actions/getTagPagePosts'
-import * as Sentry from '@sentry/browser'
-import { notFound } from 'next/navigation'
 import { AdSenseBanner } from '@components/AdSenseBanner'
-import { CategoryArticle } from '@components/CategoryArticle'
 import { Container } from '@components/Container'
 import { Loading } from '@components/LoadingCategory'
-import { Newsletter } from '@components/Newsletter'
 import { PageTitle } from '@components/PageTitle'
 import { Sidebar } from '@components/Sidebar'
 import { ad } from '@lib/ads'
 import { sharedOpenGraph } from '@lib/sharedOpenGraph'
 import { TagsPath } from '@lib/types'
 import { categoryName, titleFromSlug } from '@lib/utils'
-
-const postsQty = Number(process.env.NEXT_PUBLIC_POSTS_QTY_CATEGORY ?? 10)
+import { Content } from '@blocks/content/TagPosts'
 
 type Params = Promise<{ slug: string }>
 type SearchParams = Promise<{ [key: string]: string | string[] | undefined }>
@@ -42,39 +36,6 @@ export async function generateStaticParams() {
     tagList?.edges.map(({ node }) => ({
       slug: node.slug
     })) ?? []
-  )
-}
-
-const Content = async ({ slug }: { slug: string }) => {
-  const result = await getTagPagePosts({ slug, qty: postsQty })
-
-  if (!result?.edges) {
-    Sentry.captureException('Failed to fetch tag posts')
-    return notFound()
-  }
-
-  const { edges } = result
-
-  return (
-    <>
-      {edges.map(({ node }, index) => (
-        <Fragment key={node.id}>
-          <CategoryArticle
-            {...node}
-            isFirst={index === 0}
-            isLast={index + 1 === edges.length}
-          />
-          {index + 1 === 5 && <Newsletter className='my-4 md:hidden' />}
-          {(index + 1) % 5 === 0 && index !== edges.length - 1 && (
-            <AdSenseBanner
-              className='bloque-adv-list'
-              {...ad.category.in_article}
-            />
-          )}
-        </Fragment>
-      ))}
-      <AdSenseBanner {...ad.global.more_news} />
-    </>
   )
 }
 
