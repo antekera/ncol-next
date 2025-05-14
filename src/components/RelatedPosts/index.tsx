a1a778d6c778efbbd67830e237e49710d98dc5af
antekera <miguel.antekera@gmail.com>
2025-05-14 15:04:51 -0400

    feat(RelatedPosts): add loading skeleton for related posts
    Implement a loading skeleton component (`LeftPostsSkeleton`) to improve user experience by providing visual feedback while related posts data is being fetched. This prevents layout shifts and enhances perceived performance.

--- src/components/RelatedPosts/index.tsx
+++ src/components/RelatedPosts/index.tsx
@@ -2,29 +2,46 @@
 
 import { AdSenseBanner } from '@components/AdSenseBanner'
 import { CategoryArticle } from '@components/CategoryArticle'
+import { LeftPostsSkeleton } from '@components/LoadingHome'
 import { ad } from '@lib/ads'
 import { RECENT_NEWS } from '@lib/constants'
 import { useRelatedPosts } from '@lib/hooks/data/useRelatedPosts'
 
 const RelatedPosts = ({ slug, inView }: { slug: string; inView: boolean }) => {
-  const { data } = useRelatedPosts({ slug, enabled: inView })
-
-  if (!data || data.length < 3) {
-    return null
-  }
+  const { data, isLoading } = useRelatedPosts({ slug, enabled: inView })
 
   return (
-    <div className='mx-auto max-w-5xl sm:px-6 md:mt-12 lg:px-8'>
+    <div className='mx-auto max-w-5xl md:mt-12'>
       <h5 className='link-post-category border-primary bg-primary inline-block rounded-sm px-1 pt-1 pb-[3px] font-sans text-sm leading-none text-white uppercase'>
         {RECENT_NEWS}
       </h5>
       <hr className='mt-3 mb-4 max-w-xl border-t-2 border-gray-300 dark:border-neutral-500' />
-      <div className='grid grid-cols-3 gap-5 lg:w-11/12'>
-        {data.map(({ node }) => (
-          <div key={node.slug}>
-            <CategoryArticle {...node} type='thumbnail' excerpt={undefined} />
-          </div>
-        ))}
+      <div className='grid grid-cols-3 gap-5'>
+        {isLoading ? (
+          <>
+            <div>
+              <LeftPostsSkeleton />
+            </div>
+            <div>
+              <LeftPostsSkeleton />
+            </div>
+            <div>
+              <LeftPostsSkeleton />
+            </div>
+          </>
+        ) : (
+          <>
+            {data?.map(({ node }) => (
+              <div key={node.slug}>
+                <CategoryArticle
+                  {...node}
+                  type='thumbnail'
+                  excerpt={undefined}
+                />
+              </div>
+            ))}
+          </>
+        )}
       </div>
       <AdSenseBanner {...ad.global.related} />
       <div className='mt-16 mb-8 max-w-xl'>
