cc613823948f656d1598db8b78c8a82abae82d1c
antekera <miguel.antekera@gmail.com>
2025-05-18 10:29:57 -0400

    refactor(actions): remove unused query files and simplify getPostAndMorePosts
    Clean up the codebase by deleting unused query files and removing redundant code in the getPostAndMorePosts function to improve maintainability

--- src/app/actions/getPostAndMorePosts/index.ts
+++ src/app/actions/getPostAndMorePosts/index.ts
@@ -3,11 +3,8 @@
 import { cachedFetchAPI } from '@app/actions/fetchAPI'
 import { TIME_REVALIDATE } from '@lib/constants'
 import { SinglePost } from '@lib/types'
-import { query, queryMetaData } from './query'
+import { queryMetaData } from './query'
 
-const DRAFT = 'draft'
-const PUBLISH = 'publish'
-const DATABASE_ID = 'DATABASE_ID'
 const SLUG = 'SLUG'
 
 export const getMetadataPosts = async (
@@ -21,58 +18,6 @@ export const getMetadataPosts = async (
       idType: SLUG
     }
   })
-
-  return data
-}
-
-export const getPostAndMorePosts = async (
-  slug: string,
-  preview: boolean | undefined,
-  previewData?: any,
-  relatedSearch = 'cabimas'
-): Promise<Partial<SinglePost>> => {
-  const postPreview = preview ? previewData?.post : undefined
-  const idPreview = postPreview?.id || ''
-  const slugPreview = postPreview?.slug || ''
-  const statusPreview = postPreview?.status || ''
-  const isId = Number.isInteger(Number(slug)) // The slug is the id of the unpublished post
-  const isSamePost = isId ? slug === idPreview : slug === slugPreview
-  const isDraft = isSamePost && statusPreview === DRAFT
-  const isRevision = isSamePost && statusPreview === PUBLISH
-  const queryOptions = {
-    isRevision,
-    relatedSearch
-  }
-  const data = await cachedFetchAPI({
-    revalidate: TIME_REVALIDATE.WEEK,
-    query: query(queryOptions),
-    variables: {
-      id: isDraft ? idPreview : slug,
-      idType: isDraft ? DATABASE_ID : SLUG
-    }
-  })
-
-  if (!data?.post) {
-    return {
-      post: undefined
-    }
-  }
-
-  // Draft posts may not have an slug
-  if (isDraft) {
-    data.post.slug = idPreview
-  }
-
-  // Apply a revision (changes in a published post)
-  if (isRevision && data.post.revisions) {
-    const revision = data.post.revisions.edges[0]?.node
-
-    if (revision) {
-      Object.assign(data.post, revision)
-    }
-
-    delete data.post.revisions
-  }
 
   return data
 }
