b5a9fa02b515b1321469d10091ce1a82db712113
Miguel Antequera <miguel.antekera@gmail.com>
2026-04-25 15:25:11 -0400

    revert: PR #189 and today's visibility fixes (#191)

--- sst.config.ts
+++ sst.config.ts
@@ -41,8 +41,8 @@ export default $config({
         MAILCHIMP_API_KEY: process.env.MAILCHIMP_API_KEY ?? '',
         MAILCHIMP_API_SERVER: process.env.MAILCHIMP_API_SERVER ?? '',
         MAILCHIMP_AUDIENCE_ID: process.env.MAILCHIMP_AUDIENCE_ID ?? '',
-        TINYBIRD_TOKEN: process.env.TINYBIRD_TOKEN,
-        TINYBIRD_URL: process.env.TINYBIRD_URL,
+        TINYBIRD_TOKEN: process.env.TINYBIRD_TOKEN ?? '',
+        TINYBIRD_URL: process.env.TINYBIRD_URL ?? '',
         SITE_URL: process.env.DOMAIN_NAME ? `https://${process.env.DOMAIN_NAME}` : 'http://localhost:3000'
       }
     })
