92da4dcbbeaa9ae6ac0f916cbd3ec3098f8815f6
Miguel Antequera <miguel.antekera@gmail.com>
2026-07-02 14:05:22 -0400

    Queue GA events before GTM initializes

--- src/lib/utils/__tests__/ga.test.ts
+++ src/lib/utils/__tests__/ga.test.ts
@@ -3,6 +3,10 @@ import { EventProps, GAEvent, GAPageView, PageEventProps } from '../ga'
 const label = 'test label'
 
 describe('GAPageView', () => {
+  beforeEach(() => {
+    window.dataLayer = []
+  })
+
   it('should create a page view event with the correct properties', () => {
     const props: PageEventProps = {
       pageType: 'test',
@@ -18,9 +22,27 @@ describe('GAPageView', () => {
       pageTitle: 'Test Page'
     })
   })
+
+  it('should queue page views when GTM has not initialized dataLayer yet', () => {
+    delete (window as Partial<Window>).dataLayer
+
+    const props: PageEventProps = {
+      pageType: 'test',
+      pageUrl: 'http://test.com',
+      pageTitle: 'Test Page'
+    }
+
+    const event = GAPageView(props)
+
+    expect(window.dataLayer).toEqual([event])
+  })
 })
 
 describe('GAEvent', () => {
+  beforeEach(() => {
+    window.dataLayer = []
+  })
+
   it('should create a click event with the correct properties', () => {
     const props: EventProps = {
       category: 'test',
