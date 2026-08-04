c834238df723fc28f70aaaf2ae6a7cbe0f6e3fc8
antekera <miguel.antekera@gmail.com>
2026-08-04 16:07:09 -0400

    fix(columnistas): add trailing slash to /api/opinion/authors/ to avoid redirect

--- src/lib/hooks/data/useOpinionAuthors.ts
+++ src/lib/hooks/data/useOpinionAuthors.ts
@@ -7,7 +7,7 @@ const fetcher = (url: string) => fetch(url).then(res => res.json())
 
 export function useOpinionAuthors() {
   const { data, error, isLoading } = useSWR<OpinionAuthor[]>(
-    '/api/opinion/authors',
+    '/api/opinion/authors/',
     fetcher,
     { revalidateOnFocus: false, revalidateOnReconnect: false }
   )
